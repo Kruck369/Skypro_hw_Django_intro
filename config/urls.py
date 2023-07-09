359598a6d5336cf1a9234aafc43bccc23835dc39
Kruck <reka_931@mail.ru>
2023-07-09 11:43:47 +0300

    С учетом условий заданий ДЗ и доп. задания.

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import index, contact, categories, orders
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('contacts/', contact),
+    path('categories/', categories),
+    path('orders/', orders),
 ]
